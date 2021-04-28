8598ac85a2659dd63bf594bd0d93893fccdbcbf6
Matt Wynne <matt@cucumber.io>
2021-04-28 11:39:22 -0700

    Yay everything works!

--- src/Board.ts
+++ src/Board.ts
@@ -1,15 +1,21 @@
+class DeadCell {
+  toString() {
+    return '.'
+  }
+}
+
 export class Board {
   static from(raw: string[][]): Board {
-    return new Board(raw)
+    return new Board(raw.map((row) => row.map((rawCell) => new DeadCell())))
   }
 
-  constructor(readonly raw: string[][]) {}
+  constructor(readonly cells: DeadCell[][]) {}
 
   iterate(iterations: number): Board {
-    return new Board(this.raw)
+    return new Board(this.cells)
   }
 
   asArray(): string[][] {
-    return this.raw
+    return this.cells.map((rows) => rows.map((cell) => cell.toString()))
   }
 }
